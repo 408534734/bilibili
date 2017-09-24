d99566d23922f52e1186653b297c84dc5b2488ec
408534734 <408534734@qq.com>
2017-09-24 17:53:17 +0800

    Program Complete!

---
+++ Bili.bat
@@ -0,0 +1,1 @@
+java -jar Bilibili_server.jar
--- MySocket.py
+++ MySocket.py
@@ -9,23 +9,23 @@ class MySocket:
         self.bufferSize = 1024
         self.ADDR = (self.host, self.port)
         self.username = username
-        self.connector = socket()
 
     def get(self):
         try:
-            self.connector.connect(self.ADDR)
+            connector = socket()
+            connector.connect(self.ADDR)
             username = self.username + '\n'
-            self.connector.send(username.encode('utf8'))
-            data = self.connector.recv(self.bufferSize)
+            connector.send(username.encode('utf8'))
+            data = connector.recv(self.bufferSize)
             data = data.decode('gb2312')
-            self.connector.close()
+            #self.connector.close()
             return data
         except TimeoutError:
-            return '连接超时！'
+            return '连接超时！   '
         except ConnectionRefusedError:
-            return '连接错误！'
+            return '连接错误！   '
         else:
-            return 'UnknowError'
+            return 'UnknowError   '
 
 
 if __name__ == '__main__':
